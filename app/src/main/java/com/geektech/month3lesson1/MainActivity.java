01ed36de4bf76077b3c6726de8ed1b92b583dad0
SanzharAizhigitov <ajzigitovsanzar@gmail.com>
2022-12-09 21:48:59 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/month3lesson1/MainActivity.java
@@ -0,0 +1,52 @@
+package com.geektech.month3lesson1;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    EditText editTextSubject,editTextMessage;
+    Button send;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextSubject=(EditText)findViewById(R.id.zagalovok);
+        editTextMessage=(EditText)findViewById(R.id.soobshenie);
+
+        send=(Button)findViewById(R.id.button1);
+
+        send.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                String heading=editTextSubject.getText().toString();
+                String message=editTextMessage.getText().toString();
+
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_SUBJECT, heading);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+
+
+                email.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+
+            }
+
+        });
+    }
+
+
+
+
+}
